f49d323231a814746fb46c7d42b0be0771e8af5d
Joris Buchou <joris.buchou@gmail.com>
2023-06-12 21:02:48 -0700

    Improve Image

--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
+import GeoJpg from "./geo.jpg";
 
 const people = [
   {
     name: "Geoffrey Buchou",
     role: "Founder",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    imageUrl: GeoJpg,
   },
   // More people...
 ];
@@ -31,8 +31,10 @@ export default function Team() {
             <li key={person.name}>
               <div className="flex items-center gap-x-6">
                 <Image
-                  className="h-16 w-16 rounded-full"
+                  className="h-16 w-16 rounded-full object-cover"
                   src={person.imageUrl}
+                  width={256}
+                  height={256}
                   alt=""
                 />
                 <div>
